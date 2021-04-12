1f26544ead6438d2b65033f0c7a959943bd4b43d
TheRubble <482262+TheRubble@users.noreply.github.com>
2021-04-12 12:11:34 +0100

    Original package has been deprecated this is the replacement. (#9)
    Co-authored-by: TheRubble <therubble@hotmail.com>

--- main/src/ReCode.Cocoon.Proxy/Proxy/CocoonProxyExtensions.cs
+++ main/src/ReCode.Cocoon.Proxy/Proxy/CocoonProxyExtensions.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Diagnostics;
-using System.Net;
-using System.Net.Http;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.ReverseProxy.Service.Proxy;
 
 namespace ReCode.Cocoon.Proxy.Proxy
 {
