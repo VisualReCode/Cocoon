776609d39de0dfdfd023d4a97cf64dc8e20c326d
TheRubble <therubble@hotmail.com>
2021-07-20 07:56:03 +0100

    Working on session stuff .....

--- main/test/ReCode.Cocoon.Integration.Tests/CocoonFunctionality.cs
+++ main/test/ReCode.Cocoon.Integration.Tests/CocoonFunctionality.cs
@@ -8,15 +8,10 @@ namespace ReCode.Cocoon.Integration.Tests
 {
     public class CocoonFunctionality
     {
-        public CocoonFunctionality()
-        {
-            
-        }
-        
         [Fact]
         public async Task Pages_Available_In_Modern_App_Should_Serve_Before_Cocoon()
         {
-
+            // Arrange
             using var playwright = await Playwright.CreateAsync();
 
             await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
@@ -24,16 +19,19 @@ namespace ReCode.Cocoon.Integration.Tests
                 Headless = false
             });
 
+            // Act
             var page = await browser.NewPageAsync();
             await page.GotoAsync("http://localhost:8080");
             var result = await page.TextContentAsync("#app > div > div.main > div.content.px-4 > div > strong");
+            
+            // Assert
             result.Should().Be("How is Blazor working for you?");
         }
         
         [Fact]
         public async Task Pages_UnAvailable_In_Modern_App_Should_Serve_From_Cocoon()
         {
-
+            // Arrange
             using var playwright = await Playwright.CreateAsync();
 
             await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
@@ -41,9 +39,12 @@ namespace ReCode.Cocoon.Integration.Tests
                 Headless = false
             });
 
+            // Act
             var page = await browser.NewPageAsync();
             await page.GotoAsync("http://localhost:8080/about");
             var result = await page.TextContentAsync("#ctl01 > div.container.body-content > p");
+            
+            // Assert
             result.Should().Be("Use this area to provide additional information.");
         }
 
@@ -77,6 +78,35 @@ namespace ReCode.Cocoon.Integration.Tests
             // Assert
             result.Should().Be("Manage your account");
         }
-        
+
+        [Fact]
+        public async Task Session_should_be_shared_between_apps()
+        {
+            // Arrange
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Firefox.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false
+            });
+
+            var sessionTest = Guid.NewGuid().ToString();
+            
+            // Act
+            var page = await browser.NewPageAsync();
+            
+            // Login
+            await page.GotoAsync($"http://localhost:8080/session?sessiontext={sessionTest}");
+            
+            // Find the nav bar item, can take some time as blazor has to reinit
+            var sessionLabel = await page.WaitForSelectorAsync("#MainContent_sessionTextLabel", new PageWaitForSelectorOptions
+            {
+                Timeout = 3000
+            });
+
+            var result = await sessionLabel.InnerTextAsync();
+            
+            // Assert
+            result.Should().Be(sessionTest);
+        }
     }
 }
